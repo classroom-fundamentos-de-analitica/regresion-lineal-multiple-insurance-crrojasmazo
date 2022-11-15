7927499673c0e30b8aecb3cc184b5c98de7322c4
crrojasmazo <71391747+crrojasmazo@users.noreply.github.com>
2022-11-14 19:09:24 -0500

    half first commit

--- preguntas.py
+++ preguntas.py
@@ -18,16 +18,17 @@ def pregunta_01():
     -------------------------------------------------------------------------------------
     """
     # Lea el archivo `insurance.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv("insurance.csv")
+
 
     # Asigne la columna `charges` a la variable `y`.
-    ____ = ____
+    y = df["charges"]
 
     # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____(____)
+    X = df.copy()
 
     # Remueva la columna `charges` del DataFrame `X`.
-    ____.____(____)
+    X.drop(["charges"], axis = 1, inplace = True)
 
     # Retorne `X` y `y`
     return X, y
@@ -40,18 +41,18 @@ def pregunta_02():
     """
 
     # Importe train_test_split
-    from ____ import ____
+    from sklearn.model_selection import train_test_split
 
     # Cargue los datos y asigne los resultados a `X` y `y`.
     X, y = pregunta_01()
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 12345. Use 300 patrones para la muestra de prueba.
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
+    (X_train, X_test, y_train, y_test,) = train_test_split(
+        X,
+        y,
+        test_size = 300,
+        random_state = 12345,
     )
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
